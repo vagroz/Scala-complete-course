ed3026f618b9e25df80b5958b4529530dbb1f392
Ivan Malyshev <ivan.malyshev@phystech.edu>
2018-04-23 21:45:51 +0300

    fix unit test

--- src/main/scala/lectures/di/macwire/MacWireProgram.scala
+++ src/main/scala/lectures/di/macwire/MacWireProgram.scala
@@ -2,17 +2,18 @@ package lectures.di.macwire
 
 import java.sql.Connection
 
-import lectures.di.{ConfigurationImpl, UserServiceImpl}
+import lectures.di.{ConfigurationImpl, ConnectionManagerImpl, UserServiceImpl}
 import lectures.functions.{AnonymousUser, User}
 
 import scala.collection.mutable
 
-class MacWireProgramDependency(val connection: Connection) {
+class MacWireProgramDependency() {
   import com.softwaremill.macwire._
 
-  val configurationMap: mutable.Map[String, String] = mutable.Map("user" -> "Frosya", "password" -> "qwerty3")
+  val configurationMap: mutable.Map[String, String] = mutable.Map("user" -> "Frosya", "password" -> "qwerty3",
+    "connectionUri" -> "jdbc:sqlite:memory")
   lazy val configuration = wire[ConfigurationImpl]
-  // lazy val connectionManager = wire[ConnectionManagerImpl]
+  lazy val connectionManager = wire[ConnectionManagerImpl]
   lazy val userService = wire[UserServiceImpl]
 }
 
--- src/test/scala/lectures/di/reader/ReaderDIUserServiceTest.scala
+++ src/test/scala/lectures/di/reader/ReaderDIUserServiceTest.scala
@@ -8,32 +8,25 @@ import org.scalatest.{BeforeAndAfterAll, Matchers, WordSpec}
 
 class ReaderDIUserServiceTest extends WordSpec with Matchers with BeforeAndAfterAll {
 
-  Class.forName("org.sqlite.JDBC")
-  private val connection = DriverManager.getConnection("jdbc:sqlite:memory")
-  connection.setAutoCommit(false)
-
-  override def afterAll(): Unit = {
-    connection.close()
-  }
 
   "Reader program" should {
-    "should not find any user" in new ReaderUserServiceTestSuite(connection)({
-      val configuration = ConfigurationImpl(Map.empty[String, String])
-      val programConfig = UserServiceProgramDepsImpl(connection, configuration)
+    "should not find any user" in {
+      val configuration = ConfigurationImpl(Map("connectionUri" -> "jdbc:sqlite:memory"))
+      val programConfig = UserServiceProgramDepsImpl(configuration)
       val r = UserServiceReaderDIProgramImpl()
       val res: Option[User] = r.run(programConfig)
       res shouldBe None
-    })
+    }
 
-    "find correct user" in new ReaderUserServiceTestSuite(connection) ({
-      val configuration = ConfigurationImpl(Map.empty[String, String])
+    "find correct user" in {
+      val configuration = ConfigurationImpl(Map("connectionUri" -> "jdbc:sqlite:memory"))
       configuration.setAttribute("user", "Frosya")
       configuration.setAttribute("password", "qwerty3")
       // Именно в этом месте происходит иньекция зависимостей в наше небольшое приложение
-      val programConfig = UserServiceProgramDepsImpl(connection, configuration)
+      val programConfig = UserServiceProgramDepsImpl(configuration)
       val r = UserServiceReaderDIProgramImpl()
       val Some(res) = r.run(programConfig)
       res.id shouldBe 22
-    })
+    }
   }
 }
