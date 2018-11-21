25ea8ec7b388d4a2eef3623715b8d6eac63f1d15
Ivan Malyshev <i.malyshev@tinkoff.ru>
2018-11-21 13:34:53 +0300

    IO4Free

--- src/main/scala/lectures/cat/IO4Free.scala
+++ src/main/scala/lectures/cat/IO4Free.scala
@@ -4,6 +4,7 @@ import java.util.Scanner
 
 import cats._
 import cats.free.Free
+import scala.reflect.runtime.universe._
 
 /**
   * IO очень просто реализовать с помощью Free
@@ -14,16 +15,35 @@ import cats.free.Free
   * Так, что бы IOIsFree печатало в консоль реультат запуска program
   */
 sealed trait IO4Free[+R]
+case class Voidd(op: () => Unit) extends IO4Free[Unit]
+case class SomeType[+T](op: () => T) extends IO4Free[T]
 
 object IO4Free {
 
   import cats.free.Free._
 
   val interpreter = new (IO4Free ~> Id) {
-    override def apply[A](fa: IO4Free[A]): Id[A] = ???
+    override def apply[A](fa: IO4Free[A]): Id[A] = {
+      fa match {
+        case Voidd(op) =>
+          op()
+        case SomeType(op) =>
+          op()
+      }
+    }
   }
 
-  implicit def toIO[A](f: () => A): Free[IO4Free, A] = ???
+  implicit def toIO[A: TypeTag](f: () => A): Free[IO4Free, A] = {
+
+    val io: IO4Free[A] = f match {
+      case f if typeOf[A] =:= typeOf[Unit] =>
+        Voidd(f.asInstanceOf[Function0[Unit]]).asInstanceOf[IO4Free[A]]
+      case f: Function0[A] =>
+        SomeType[A](f)
+    }
+    toFree[A](io)
+  }
+
   implicit def toFree[A](iof: IO4Free[A]): Free[IO4Free, A] = liftF(iof)
 
   def computationBasedOnInput(i: Int): Free[IO4Free, String] = for {
